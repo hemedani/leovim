a6900d41c8afdab1d8cac990601bb3cf40048f59
Syd Amir <hemedani@gmail.com>
2021-06-27 17:45:03 +0430

    add post install command to live server in bracey

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -130,7 +130,7 @@ return require("packer").startup(function(use)
     require_plugin('dial.nvim')
     use {'nacro90/numb.nvim', opt = true}
     require_plugin('numb.nvim')
-    use {'turbio/bracey.vim', opt = true}
+    use {'turbio/bracey.vim', opt = true, run="npm install --prefix server"}
     require_plugin('bracey.vim')
     use {'phaazon/hop.nvim', opt = true}
     require_plugin('hop.nvim')
